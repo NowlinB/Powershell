6a6cf82151f65c515b19d42145dac90cb705f8b6
Brandon Nowlin <38094031+NowlinB@users.noreply.github.com>
2025-04-27 18:41:32 -0500

    Uploaded Display-IP-Address-Information.ps1
    This Powershell scripts uses the IP-API to retrieve information about the user provided IP address https://ip-api.com/

---
+++ Display-IP-Address-Information.ps1
@@ -0,0 +1,12 @@
+﻿<# 
+This Powershell script displays information in XML format about a user provided IP address
+This script Utilizes ip-api.com to display the results of a HTTP GET request to the ip-api for the specified IP address
+Created by Brandon Nowlin
+#>
+try{
+    $IPaddress = Read-Host "Please enter an IP address"
+    [xml]$IpAddressInfo = Invoke-RestMethod -Method Get -Uri "http://ip-api.com/xml/$IPaddress"
+    Write-Output $IpAddressInfo.OuterXml
+}catch{
+    Write-Host "There was an error"
+}
