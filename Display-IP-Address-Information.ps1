db2863e47823cdd8f66f7c3ccc142c2027bad60d
Brandon Nowlin <38094031+NowlinB@users.noreply.github.com>
2025-04-28 12:05:50 -0500

    Updated Display-IP-Address-Information.ps1
    Added input checking to ensure that the user submitted read-host value is a valid IPv4 or IPv6 address before submitting the request to  ip-api

--- Display-IP-Address-Information.ps1
+++ Display-IP-Address-Information.ps1
@@ -1,12 +1,15 @@
-﻿<# 
+<#
 This Powershell script displays information in XML format about a user provided IP address
 This script Utilizes ip-api.com to display the results of a HTTP GET request to the ip-api for the specified IP address
 Created by Brandon Nowlin
 #>
-try{
-    $IPaddress = Read-Host "Please enter an IP address"
+
+$IPaddress = Read-Host "Please enter an IP address"
+#Emsure the IPv4 or IPv6 address is valid
+if(($IPaddress -match '^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$') -or ($IPaddress -match '((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))')
+){
     [xml]$IpAddressInfo = Invoke-RestMethod -Method Get -Uri "http://ip-api.com/xml/$IPaddress"
     Write-Output $IpAddressInfo.OuterXml
-}catch{
-    Write-Host "There was an error"
+}else{
+    Write-Host "Error you have inputted an incorrect IPv4 or IPv6 address"
 }
